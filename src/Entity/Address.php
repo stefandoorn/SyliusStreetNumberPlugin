b384cc2d1572a650ed7d4ab3d01f9880d6c72d34
Stefan Doorn <stefan@efectos.nl>
2018-12-17 09:18:34 +0100

    Use proper trait name

--- src/Entity/Address.php
+++ src/Entity/Address.php
@@ -3,10 +3,10 @@
 namespace StefanDoorn\SyliusStreetNumberPlugin\Entity;
 
 use StefanDoorn\SyliusStreetNumberPlugin\Entity\Interfaces\AddressInterface;
-use StefanDoorn\SyliusStreetNumberPlugin\Entity\Traits\HouseNumber;
+use StefanDoorn\SyliusStreetNumberPlugin\Entity\Traits\StreetNumber;
 use Sylius\Component\Core\Model\Address as BaseAddress;
 
 class Address extends BaseAddress implements AddressInterface
 {
-    use HouseNumber;
+    use StreetNumber;
 }
