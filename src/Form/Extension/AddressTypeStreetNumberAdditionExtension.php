fd6c13806287b5a74946e5dc69b3157deae1b3c3
Stefan Doorn <stefan@efectos.nl>
2019-01-10 22:09:48 +0100

    Move addition field also to subscriber and fix service injection

--- src/Form/Extension/AddressTypeStreetNumberAdditionExtension.php
+++ src/Form/Extension/AddressTypeStreetNumberAdditionExtension.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace StefanDoorn\SyliusStreetNumberPlugin\Form\Extension;
 
 use Sylius\Bundle\AddressingBundle\Form\Type\AddressType;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -13,6 +14,14 @@ use Symfony\Component\Form\FormEvents;
 
 final class AddressTypeStreetNumberAdditionExtension extends AbstractTypeExtension
 {
+    /** @var EventSubscriberInterface */
+    private $appendAdditionDataToStreetFieldEventSubscriber;
+
+    public function __construct(EventSubscriberInterface $appendAdditionDataToStreetFieldEventSubscriber)
+    {
+        $this->appendAdditionDataToStreetFieldEventSubscriber = $appendAdditionDataToStreetFieldEventSubscriber;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('addition', TextType::class, [
@@ -21,21 +30,7 @@ final class AddressTypeStreetNumberAdditionExtension extends AbstractTypeExtensi
             'validation_groups' => ['sylius', 'sylius_shipping_address_update'],
         ]);
 
-        $builder->addEventListener(FormEvents::PRE_SUBMIT,
-            function (FormEvent $event) {
-                $data = $event->getData();
-
-                $street = $data['street'];
-                $streetNumberAddition = $data['addition'];
-
-                // Add housenumber to street field to keep things compatible with Sylius
-                if (false !== strrpos($street, $streetNumberAddition)) {
-                    return;
-                }
-
-                $data['street'] = sprintf('%s %s', $street, $streetNumberAddition);
-                $event->setData($data);
-            });
+        $builder->addEventSubscriber($this->appendAdditionDataToStreetFieldEventSubscriber);
     }
 
     public function getExtendedType()
--- src/Form/Extension/AddressTypeStreetNumberExtension.php
+++ src/Form/Extension/AddressTypeStreetNumberExtension.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace StefanDoorn\SyliusStreetNumberPlugin\Form\Extension;
 
 use Sylius\Bundle\AddressingBundle\Form\Type\AddressType;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
